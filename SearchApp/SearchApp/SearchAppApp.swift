4a5386659e9184ca2dd8c66e7d5ef054a918db98
Daniil Dorofieiev <daniil.dorofieiev_ext@scout24.com>
2022-09-12 18:14:30 +0200

    init project

---
+++ SearchApp/SearchApp/SearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppApp.swift
+//  SearchApp
+//
+//  Created by Daniil Dorofieiev on 12.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
